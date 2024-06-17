74548be5247bbae6585690e016946a6e2433624e
Ian Flurkey <ianflurkey@gmail.com>
2024-06-17 15:07:26 -0400

    final commit

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      elements = nodes_to_visit.pop()
+      if elements['id'] == id:
+        return elements
+      
+      nodes_to_visit = nodes_to_visit + elements['children']
+
+    return None
+      
